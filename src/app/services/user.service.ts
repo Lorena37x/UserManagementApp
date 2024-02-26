3708d4151ed1a2fb79af5cbf286f0222b556e47a
Lorena37x <lorena.papesa37@gmail.com>
2024-02-26 15:07:56 +0100

    user and nutrition details

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,8 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
+  baseUrl: any;
 
   constructor(private _http: HttpClient) {}
+
+  getUserData(id: number): Observable<any> {
+    return this._http.get(`http://localhost:3000/users/${id}`);
+  }
+
+  getNutritionData(id: number): Observable<any> {
+    return this._http.get<any>(`http://localhost:3000/prehrana?userId=${id}`);
+  }
+
+  getUserList(): Observable<any> {
+    return this._http.get('http://localhost:3000/users');
+  }
 
   addUser(data: any): Observable<any> {
     return this._http.post('http://localhost:3000/users', data);
@@ -15,10 +28,6 @@ export class UserService {
 
   updateUser(id: number, data: any): Observable<any> {
     return this._http.put(`http://localhost:3000/users/${id}`, data);
-  }
-
-  getUserList(): Observable<any> {
-    return this._http.get('http://localhost:3000/users');
   }
 
   deleteUser(id: number): Observable<any> {
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  userId!: number;
+  userData: any;
+  nutritionInfo: any;
 
+  constructor(private route: ActivatedRoute, 
+              private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.userId = +params['userId'];
+      this.getUserData();
+    });
+  }
+
+  getUserData() {
+    this.userService.getUserData(this.userId).subscribe(
+      (user: any) => {
+        this.userData = user;
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Error fetching user data:', error);
+      }
+    );
+
+    this.userService.getNutritionData(this.userId).subscribe(
+      (nutrition: any) => {
+        this.nutritionInfo = nutrition;
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Error fetching nutrition data:', error);
+      }
+    );
+  }
 }
